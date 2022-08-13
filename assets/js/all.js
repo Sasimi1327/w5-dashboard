d772a5ab57f6a3eb8a193a89319b4052cfa93e5d
sasimi1327 <wonderland1327@gmail.com>
2022-08-14 01:48:10 +0800

    Update 2022-08-13T17:48:05.199Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,11 +1,13 @@
+"use strict";
+
 $(document).ready(function () {
   $(".nav-link").click(function () {
     $(".navList a").not(this).removeClass("active");
     $(this).toggleClass("active");
   });
-
   $(".page-link").click(function () {
     $(".page-item a").not(this).removeClass("active");
     $(this).toggleClass("active");
   });
 });
+//# sourceMappingURL=all.js.map
